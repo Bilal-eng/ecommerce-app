c6a92ae1f616b99b904156bd96c9581bf6038b88
Bilal-PC <bilal-pc@Bilal-PCs-MacBook-Pro.local>
2025-05-01 09:54:23 +0300

    Add App Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-    <div className='text-blue-400 text-center'>
-      <h1>E-Commerce App</h1>
-    </div>
+    <Router>
+      <nav className="bg-gray-800 text-white p-4 flex justify-between">
+        <Link to="/" className="font-bold text-lg">ShopEasy</Link>
+        <Link to="/cart" className="hover:underline">Cart</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
   );
 }
 
